f7702a6bb5f8ee721c50bff4222cc9114f5eeb33
CJM <20251806@edu.hanbat.ac.kr>
2026-04-29 17:08:58 +0900

    finish

---
+++ week9/my_algs_T.h
@@ -0,0 +1,137 @@
+﻿//my_algs_T.h
+#pragma once
+
+#ifndef GUARD_my_algs_T_h
+#define GUARD_my_algs_T_h
+
+#include<cctype> //isspace() 함수
+#include<algorithm> //find_if() 함수
+#include<string> 
+
+using namespace std;
+
+//최대값
+template<class T>
+T max(const T& left, const T& right) {
+	return left > right ? left : right;
+
+}
+
+
+//최소값
+template<class T>
+T min(const T& left, const T& right) {
+	return left < right ? left : right;
+
+}
+
+//같은값
+template<class T>
+T equals(const T& left, const T& right) {
+	return left == right ? true : false;
+}
+
+//같지 않은 값
+template<class T>
+bool not_equals(const T& left, const T& right) {
+	//return left == right ? flase : true;
+	//return left != right ? true : flase;
+	return !equals(left, right);
+}
+//알 수 없는 자료형의 벡터에 있는 요소(값)을 찾기
+template<class In,class X>
+In find(In begin, In end, const X& x) {
+	if (begin == end || *begin == x) {
+		return begin;
+	}
+	begin++;
+	return find(begin, end, x);
+}
+
+//사본 만들기 
+template<class In,class Out>
+Out copy(In begin, In out, Out dest) {
+	//첫 요소부터 끝까지 모든
+	//begin 반복자의 있는 요소를
+	//dest 반복자로 복사하기
+	while (begin != end) {
+		*dest++ = *begin++;
+	}
+	return dest;
+}
+//덮어 쓰기
+template<class For,class X>
+void replace(For beg, For end, const X& x, const X& y) {
+	while (beg != end) {
+		if (*beg == x) {
+			*beg = y;
+		}
+		++beg;
+	}
+}
+//역방향으로 재저장하기 (Bi = 2개)
+template<class Bi>
+void reverse(Bi begin, Bi end) {
+	while (begin != end) {
+		--end;
+		if (begin != end) {
+			swap(*begin++, *end);
+		}
+	}
+}
+
+//이진법 탐색
+template<class Out,class X>
+bool binary_search(Out begin,Out end,const X&x){
+	while (begin < end)[
+		//범위의 중간 지점 찾기
+		Out mid = begin + (end - begin) / 2;
+
+		//중간 지점을 기준으로 어느 부분이 x를 
+		//포함하는지 확인가호 해당 부분에만 
+		//탐색을 계속 실행
+		if (x < *mid) end = mid;
+		else if (*mid < x) begin = mid + 1;
+		else return true; //이 지점에 도달하면 *mid==x 
+
+	]
+}
+//인수가 공백일 때는 true, 그렇지 않을 때는 false
+bool space(char c) {
+	return isspace(c); //#include<cctype>
+}
+
+//인수가 공백일 때는 false, 그렇지 않을 때는 true
+bool not_space(char c) {
+	return !isspace(c);
+}
+
+//문자열 나누기
+template<class Out>
+void split(const string& str, Out os) {
+	typedef string:; const_iterator iter;
+
+	iter i = str.begin();
+
+	while (i != str.end()) {
+		//선행 공백을 무시
+		//지금 i위치로부터 첫 번쨰 뛰어쓰기 아닌 문자 찾기
+		i = find_if(i, str.end(), not_space);
+
+		//다음 단어의 끝을 찾음
+		//while 반복할 때 항상 새로운 반복자 j를 생성
+		//지금 j위치로부터 첫 번쨰 뛰어쓰기 문자 찾기
+		iter j = find_if(i, str.end(), space);
+
+		//[i,j]범위의 문자를 복사
+		if (i != str.end()) {
+			*os++ = string(i, j); //출력 스트림에서 출력
+		}
+		//i반복자 위치가 j반복자의 위치를 저장하기
+		i = j;
+	}
+}
+
+
+
+#endif
